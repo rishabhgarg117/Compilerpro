40dd0684ae2a0af347f6513846787d3e2f0eef06
Rishabh Garg <158310747+rishabhgarg117@users.noreply.github.com>
2025-04-19 14:25:43 +0530

    Update lexerf.c

--- lexerf.c
+++ lexerf.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <ctype.h>
 
-
 typedef enum {
 	INT,
 	KEYWORD,
@@ -18,21 +17,35 @@ typedef struct{
 
 void print_token(Token token){
     printf("TOKEN VALUE: ");
+    printf("'");
     for(int i=0; token.value[i] != '\0' ; i++){
         printf("%c", token.value[i]);
     }
- 
-    if(token.type == INT){
-        printf(" TOKEN TYPE: INT\n");
+    printf("'");
+    switch(token.type){
+        case INT:
+            printf("TOKEN TYPE: INT\n");
+            break;
+        case KEYWORD:
+            printf("TOKEN TYPE: KEYWORD\n");
+            break;
+        case SEPARATOR:
+            printf("TOKEN TYPE: SEPARATOR\n");
+            break;
+        case END_OF_TOKENS:
+            printf("END OF TOKENS");
     }
+    // if(token.type == INT){
+    //     printf(" TOKEN TYPE: INT\n");
+    // }
 
-    if(token.type == KEYWORD){
-        printf(" TOKEN TYPE: KEYWORD\n");
-    }
+    // if(token.type == KEYWORD){
+    //     printf(" TOKEN TYPE: KEYWORD\n");
+    // }
 
-    if(token.type == SEPARATOR){
-        printf(" TOKEN TYPE: SEPARATOR\n");
-    }
+    // if(token.type == SEPARATOR){
+    //     printf(" TOKEN TYPE: SEPARATOR\n");
+    // }
 }
 
 Token *generate_number(char *current, int *current_index)
@@ -52,9 +65,9 @@ Token *generate_number(char *current, int *current_index)
         *current_index += 1;
     }
     value[value_index] = '\0';
-    int dig = isdigit(current[*current_index]);
-    printf("is digit: %d | is end of line: %d\n", dig, current[*current_index] != '/0');
-    printf("VALUE INDEX: %d\n", value_index);
+    /*int dig = isdigit(current[*current_index]);
+    printf("is digit: %d_| is end of line: %d\n", dig, current[*current_index] != '\0');
+    printf("VALUE INDEX: %d\n", value_index);*/
     token->value = value; //atoi(value);
     return token;
 }
@@ -80,61 +93,79 @@ Token *generate_keyword(char *current, int *current_index)
     return token;
 }
 
+Token *generate_separator(char *current, int *current_index){
+    Token *token = malloc(sizeof(Token));
+    token->value = malloc(sizeof(char) * 2);
+    token->value[0] = current[*current_index];
+    token->value[1] = '\0';
+    token->type = SEPARATOR;
+    return token;
+}
+
 size_t tokens_index ;
 
 Token *lexer(FILE *file)
 {
     int length;
-    char *buffer = 0;
+    char *current = 0;
     fseek(file, 0, SEEK_END);
     length = ftell(file);
     fseek(file, 0, SEEK_SET);
-    buffer = malloc(sizeof(char) * length);
-    fread(buffer, 1, length, file);
+
+    current = malloc(sizeof(char) * length);
+    fread(current, 1, length, file);
+
     fclose(file);
-    buffer[length + 1] = '\0';
-    char *current = malloc(sizeof(char) * length + 1);
-    current = buffer;
+
+    current[length + 1] = '\0';
+    // char *current = malloc(sizeof(char) * length + 1);
+    // current = buffer;
     int current_index = 0;
 
-    Token *tokens = malloc(sizeof(Token)*1024);
+    Token *tokens = malloc(sizeof(Token));
     tokens_index = 0;
 
     while (current[current_index] != '\0'){
         Token *token = malloc(sizeof(Token));
-        printf("current_index: %d\n", current_index);
+        /*printf("current_index: %d\n", current_index);*/
         if (current[current_index] == ';')
         {
-            token = malloc(sizeof(Token));
-            token->value[0] = current[current_index];
-            token->value[1] = '\0';
-            token->type = SEPARATOR;
+            token= generate_separator(current, &current_index);
+            /*token = malloc(sizeof(Token));*/
+            // token->value = malloc(sizeof(char) * 2);
+            // token->value[0] = current[current_index];
+            // token->value[1] = '\0';
+            // token->type = SEPARATOR;
             tokens[tokens_index] = *token;
             tokens_index++;
         }
         else if (current[current_index] == '(')
         {
-            token = malloc(sizeof(Token));
-            token->value[0] = current[current_index];
-            token->value[1] = '\0';
-            token->type = SEPARATOR;
+            token= generate_separator(current, &current_index);
+            /*token = malloc(sizeof(Token));*/
+            // token->value = malloc(sizeof(char) * 2);
+            // token->value[0] = current[current_index];
+            // token->value[1] = '\0';
+            // token->type = SEPARATOR;
             tokens[tokens_index] = *token;
             tokens_index++;
             //print_token(*open_paren_token);
         }
         else if (current[current_index] == ')')
         {
-            token = malloc(sizeof(Token));
-            token->value[0] = current[current_index];
-            token->value[1] = '\0';
-            token->type = SEPARATOR;
+            token= generate_separator(current, &current_index);
+            /*token = malloc(sizeof(Token));*/
+            // token->value = malloc(sizeof(char) * 2);
+            // token->value[0] = current[current_index];
+            // token->value[1] = '\0';
+            // token->type = SEPARATOR;
             tokens[tokens_index] = *token;
             tokens_index++;
         }
         else if (isdigit(current[current_index]))
         {
             token = generate_number(current, &current_index); 
-            printf("TOKEN VALUE: %s\n", token->value);
+            // printf("TOKEN VALUE: %s\n", token->value);
             tokens[tokens_index] = *token;
             tokens_index++;
             current_index--;
@@ -142,15 +173,21 @@ Token *lexer(FILE *file)
         else if (isalpha(current[current_index]))
         {
             token = generate_keyword(current, &current_index);
-            print_token(*token);
+            //print_token(*token);
             tokens[tokens_index] = *token;
             tokens_index++;
             current_index--;
         }
+        // else if(current[current_index] ==' ' || current[current_index]=='\t' || current[current_index] =='\n'){
+        //     continue;
+        // }
+        // else{
+        //     printf("UNKNOWN CHARACTER :%c",current[current_index]);
+        //     exit(1);
+        // }
         free(token);
         current_index++;
     }
-
     tokens[tokens_index].value = '\0';
     tokens[tokens_index].type = END_OF_TOKENS;
     return tokens;
